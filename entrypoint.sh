e57005fa3092abfbe152f6e9e14e11ebe2d1cc61
jarrysix <jarrysix@gmail.com>
2020-12-16 16:50:03 +0800

    Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
+echo "${PLUGIN_SOURCE}"
+echo "${PLUGIN_ARGS}"
 cd "${PLUGIN_SOURCE}"
-flutter build apk "${PLUGIN_ARGS}"
+sh -c "flutter build apk ${PLUGIN_ARGS}"
 echo "build success."
+ls -al .
 ls -al "${PLUGIN_SOURCE}"/build/app/outputs/apk
