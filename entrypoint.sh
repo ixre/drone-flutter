224bc52c80708dd7b72dde7ee302df819e91eb4f
jarrysix <jarrysix@gmail.com>
2020-12-16 17:28:34 +0800

    Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,6 @@ echo "---- list source folder ----"
 ls -al .
 sh -c "flutter build apk ${PLUGIN_ARGS}"
 echo "build success."
+
+echo "---- list output folder ----"
 ls -al "${PLUGIN_SOURCE}"/build/app/outputs/apk
