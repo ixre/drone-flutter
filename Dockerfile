bc14a03c7659d9df6ceb778dd9c7e421467abfd9
jarrysix <jarrysix@gmail.com>
2020-12-16 17:53:32 +0800

    PLUGIN_CN

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM mobiledevops/flutter-sdk-image:v1.12.13-hotfix.8
 
 ENV PLUGIN_SOURCE="."
 ENV PLUGIN_ARGS=""
+ENV PLUGIN_CN=false
 
 COPY entrypoint.sh /
 RUN flutter upgrade && flutter --version
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 
 set -e
+
+if [ ! $(PLUGIN_CN) = "true" ];then
+  export PUB_HOSTED_URL=https://pub.flutter-io.cn
+  export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
+fi
 cd "${PLUGIN_SOURCE}"
 echo "---- list source folder ----"
 ls -al .
