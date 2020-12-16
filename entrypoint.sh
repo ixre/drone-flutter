3c6a535a075936f39bc89beb58261d0463a05ced
jarrysix <jarrysix@gmail.com>
2020-12-16 22:15:14 +0800

    Dockerfile user

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ ! "${PLUGIN_CN}" = "true" ];then
+if [ "${PLUGIN_CN}" = "true" ];then
   echo "use flutter chinese mirror"
   export PUB_HOSTED_URL=https://pub.flutter-io.cn
   export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
