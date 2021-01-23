1d3112f7921a6804bb404d760a8a0e6789d88981
jarrysix <jarrysix@gmail.com>
2021-01-24 06:36:42 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ if [ "${PLUGIN_CN_NET}" = "true" ];then
   export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
 fi
 if [ "${PLUGIN_PUB_CACHE}" != "" ];then
-  if [[ "${PLUGIN_PUB_CACHE}" = ~^/.* ]];then
+  if [[ "${PLUGIN_PUB_CACHE}" != /* ]];then
     echo "[ drone-flutter][ warning]: PUB_CACHE must starts with \'/\', otherwise can not be cached."
   fi
   echo "[ drone-flutter][ info]: use ${PLUGIN_PUB_CACHE} as flutter pub cache folder..."
